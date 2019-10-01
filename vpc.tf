67b61f4ec500d01035b776d14b45c5a553638eb8
Hyeon Kim <simnalamburt@gmail.com>
2019-10-02 01:33:19 +0900

    vpc.tf: Define default subnets

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,15 @@
-data "aws_vpc" "default" {
-  default = true
+locals {
+  availability_zones = {
+    a = "ap-northeast-1a",
+    c = "ap-northeast-1c",
+    d = "ap-northeast-1d",
+  }
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default" {
+  for_each = local.availability_zones
+
+  availability_zone = each.value
 }
