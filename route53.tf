4801d190f5eb02640039f2fde83f4273531a6cb0
lens0021 <lorentz0021@gmail.com>
2019-11-01 21:32:53 +0900

    Comment out temporarily

--- route53.tf
+++ route53.tf
@@ -15,13 +15,13 @@ resource "aws_route53_record" "femiwiki_com" {
   }
 }
 
-resource "aws_route53_record" "test_femiwiki_com" {
-  name    = "test.femiwiki.com"
-  type    = "A"
-  ttl     = "300"
-  zone_id = aws_route53_zone.femiwiki_com.zone_id
-  records = [aws_instance.mediawiki_green.public_ip]
-}
+# resource "aws_route53_record" "test_femiwiki_com" {
+#   name    = "test.femiwiki.com"
+#   type    = "A"
+#   ttl     = "300"
+#   zone_id = aws_route53_zone.femiwiki_com.zone_id
+#   records = [aws_instance.mediawiki_green.public_ip]
+# }
 
 resource "aws_route53_record" "femiwiki_com_amazonses_verification_record" {
   name    = "_amazonses.femiwiki.com"
