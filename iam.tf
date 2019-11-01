98312353c16609f9fcd27d8db939f6a870f1aca8
lens0021 <lorentz0021@gmail.com>
2019-11-01 21:36:43 +0900

    Remove resources for Packer

--- iam.tf
+++ iam.tf
@@ -8,7 +8,6 @@ locals {
   ]
   programmatic_users = [
     "femiwiki-email",
-    "packer",
   ]
 }
 
@@ -248,62 +247,6 @@ data "aws_iam_policy_document" "upload_backup" {
   statement {
     actions   = ["s3:PutObject"]
     resources = ["${aws_s3_bucket.backups.arn}/*"]
-  }
-}
-
-resource "aws_iam_user_policy_attachment" "packer" {
-  user       = "packer"
-  policy_arn = aws_iam_policy.packer.arn
-}
-
-resource "aws_iam_policy" "packer" {
-  name        = "Packer"
-  description = "Policy for packer"
-  path        = "/"
-  policy      = data.aws_iam_policy_document.packer.json
-}
-
-data "aws_iam_policy_document" "packer" {
-  statement {
-    actions = [
-      "ec2:AttachVolume",
-      "ec2:AuthorizeSecurityGroupIngress",
-      "ec2:CopyImage",
-      "ec2:CreateImage",
-      "ec2:CreateKeypair",
-      "ec2:CreateSecurityGroup",
-      "ec2:CreateSnapshot",
-      "ec2:CreateTags",
-      "ec2:CreateVolume",
-      "ec2:DeleteKeyPair",
-      "ec2:DeleteSecurityGroup",
-      "ec2:DeleteSnapshot",
-      "ec2:DeleteVolume",
-      "ec2:DeregisterImage",
-      "ec2:DescribeImageAttribute",
-      "ec2:DescribeImages",
-      "ec2:DescribeInstances",
-      "ec2:DescribeRegions",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeSnapshots",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeTags",
-      "ec2:DescribeVolumes",
-      "ec2:DetachVolume",
-      "ec2:GetPasswordData",
-      "ec2:ModifyImageAttribute",
-      "ec2:ModifyInstanceAttribute",
-      "ec2:ModifySnapshotAttribute",
-      "ec2:RegisterImage",
-      "ec2:RunInstances",
-      "ec2:StopInstances",
-      "ec2:TerminateInstances",
-      "ec2:RequestSpotInstances",
-      "ec2:CancelSpotInstanceRequests",
-      "ec2:DescribeSpotInstanceRequests",
-      "ec2:DescribeSpotPriceHistory"
-    ]
-    resources = ["*"]
   }
 }
 
