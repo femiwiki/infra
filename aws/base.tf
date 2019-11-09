f98522c1d42e1e22230627571474fa78ee0460cc
lens0021 <lorentz0021@gmail.com>
2019-11-09 15:29:56 +0900

    Split into two workspaces
    Issue: https://github.com/femiwiki/infra/issues/11

---
+++ aws/base.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">=0.12.9, <0.13"
+
+  backend "remote" {
+    organization = "femiwiki"
+
+    workspaces {
+      name = "infra"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+  version = ">= 2.30.0, < 3"
+}
+
+provider "aws" {
+  alias   = "us"
+  region  = "us-east-1"
+  version = ">= 2.30.0, < 3"
+}
