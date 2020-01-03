a110b1d4b3477c06bb72169535cc21271256b1c6
Hyeon Kim <simnalamburt@gmail.com>
2020-01-04 02:51:39 +0900

    sns.tf: New SNS topic, alarm SNS for us-east-1 region

--- aws/sns.tf
+++ aws/sns.tf
@@ -1,6 +1,18 @@
+# NOTE: email에 대한 AWS SNS topic subscription은 테라폼으로 적용할 수 없습니다.
+# 수동으로 설정해 주세요.
+#
+# Reference:
+#   https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html#email
+
 resource "aws_sns_topic" "cloudwatch_alarms_topic" {
   name   = "CloudWatch_Alarms_Topic"
   policy = data.aws_iam_policy_document.sns_default_policy.json
+}
+
+resource "aws_sns_topic" "cloudwatch_alarms_topic_us" {
+  provider = aws.us
+  name     = "CloudWatch_Alarms_Topic"
+  policy   = data.aws_iam_policy_document.sns_default_policy.json
 }
 
 data "aws_iam_policy_document" "sns_default_policy" {
@@ -17,15 +29,17 @@ data "aws_iam_policy_document" "sns_default_policy" {
       "SNS:Receive",
     ]
 
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+      values   = ["302617221463"]
+    }
+
     principals {
       type        = "AWS"
       identifiers = ["*"]
     }
 
     resources = ["arn:aws:sns:*"]
-    sid = "CloudwatchAlarmsTopic"
   }
 }
-
-# email에 대한 Aws sns topic subscription은 테라폼을로 적용할 수 없습니다. 수동으로 설정해 주세요.
-# See https://www.terraform.io/docs/providers/aws/r/sns_topic_subscription.html#email
