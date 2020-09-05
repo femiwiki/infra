f078fce3fbe6b0d1bc9072c0feb213501ba87e68
lens0021 <lorentz0021@gmail.com>
2020-09-05 19:16:19 +0900

    Create Package Manager team
    Related to https://github.com/femiwiki/femiwiki/issues/97

--- github/team.tf
+++ github/team.tf
@@ -1,4 +1,11 @@
 resource "github_team" "reviewer" {
-  name    = "Reviewer"
-  privacy = "closed"
+  name        = "Reviewer"
+  description = "People reviwing PRs"
+  privacy     = "closed"
+}
+
+resource "github_team" "package_manager" {
+  name        = "Package Manager"
+  description = "People managing Github Packages and Github Container Registry"
+  privacy     = "closed"
 }
