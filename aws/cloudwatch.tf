996e571d0599452e6693be5056eb6d88f03c3f49
Hyeon Kim <simnalamburt@gmail.com>
2020-12-27 00:40:43 +0900

    cloudwatch.tf: New cloudwatch metric alarm, 'femiwiki_main_page'

--- aws/cloudwatch.tf
+++ aws/cloudwatch.tf
@@ -61,3 +61,21 @@ resource "aws_cloudwatch_metric_alarm" "complaint_rate" {
   alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic_us.arn]
   treat_missing_data  = "ignore"
 }
+
+resource "aws_cloudwatch_metric_alarm" "femiwiki_main_page" {
+  provider = aws.us
+
+  alarm_name  = "Femiwiki_Main_Page-awsroute53-65854b5e-a8cb-40f7-87ba-5d3055f9effd-Low-HealthCheckStatus"
+  metric_name = "HealthCheckStatus"
+  namespace   = "AWS/Route53"
+  period      = 60
+  statistic   = "Minimum"
+  dimensions = {
+    HealthCheckId = "65854b5e-a8cb-40f7-87ba-5d3055f9effd"
+  }
+  evaluation_periods  = "1"
+  threshold           = 1
+  comparison_operator = "LessThanThreshold"
+  datapoints_to_alarm = 1
+  alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic_us.arn]
+}
