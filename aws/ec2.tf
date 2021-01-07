9ea077422339f22b0bc6c193b927ddac2d23ca19
lens0021 <lorentz0021@gmail.com>
2021-01-07 12:19:50 +0900

    terraform fmt

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -103,10 +103,10 @@ resource "aws_eip" "femiwiki" {
 }
 
 resource "aws_instance" "femiwiki_green" {
-  ebs_optimized           = true
-  ami                     = data.aws_ami.amazon_linux_2.image_id
-  instance_type           = "t3a.small"
-  key_name                = aws_key_pair.femiwiki_green.key_name
+  ebs_optimized = true
+  ami           = data.aws_ami.amazon_linux_2.image_id
+  instance_type = "t3a.small"
+  key_name      = aws_key_pair.femiwiki_green.key_name
   # During expremental period
   hibernation             = true
   monitoring              = false
