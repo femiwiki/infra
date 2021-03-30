e2023d86b649360de9185090f81b84d32ee35b22
lens0021 <lorentz0021@gmail.com>
2021-03-30 23:36:48 +0900

    aws: Add CloudWatch alarms for femiwiki_green

--- aws/cloudwatch.tf
+++ aws/cloudwatch.tf
@@ -49,6 +49,57 @@ resource "aws_cloudwatch_metric_alarm" "femiwiki_disk_used_cloud_watch_alarm" {
   alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic.arn]
 }
 
+resource "aws_cloudwatch_metric_alarm" "femiwiki_green_cpu_credit_balance_cloud_watch_alarm" {
+  alarm_name  = "Femiwiki CPU credit balance cloud watch alarm"
+  namespace   = "AWS/EC2"
+  metric_name = "CPUCreditBalance"
+  period      = 300
+  statistic   = "Minimum"
+  dimensions = {
+    InstanceId = aws_instance.femiwiki_green.id
+  }
+  evaluation_periods  = "1"
+  threshold           = "108"
+  comparison_operator = "LessThanThreshold"
+  datapoints_to_alarm = 1
+  alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "femiwiki_green_burst_balance_cloud_watch_alarm" {
+  alarm_name  = "Femiwiki Burst Balance cloud watch alarm"
+  namespace   = "AWS/EBS"
+  metric_name = "BurstBalance"
+  period      = 300
+  statistic   = "Minimum"
+  dimensions = {
+    VolumeId = aws_instance.femiwiki_green.root_block_device.0.volume_id
+  }
+  evaluation_periods  = "1"
+  threshold           = "75"
+  comparison_operator = "LessThanThreshold"
+  datapoints_to_alarm = 1
+  alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "femiwiki_green_disk_used_cloud_watch_alarm" {
+  alarm_name  = "Femiwiki disk used cloud watch alarm"
+  namespace   = "CWAgent"
+  metric_name = "disk_used_percent"
+  period      = 300
+  statistic   = "Maximum"
+  dimensions = {
+    "device" = "nvme0n1p1"
+    "fstype" = "xfs"
+    "host"   = aws_instance.femiwiki_green.private_dns
+    "path"   = "/"
+  }
+  evaluation_periods  = "1"
+  threshold           = "90"
+  comparison_operator = "GreaterThanThreshold"
+  datapoints_to_alarm = 1
+  alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic.arn]
+}
+
 resource "aws_cloudwatch_metric_alarm" "bounce_rate" {
   provider = aws.us
 
