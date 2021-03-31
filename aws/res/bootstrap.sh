a514cbdbfce9dfa04bb934dd7e9c804ecf461cc4
lens0021 <lorentz0021@gmail.com>
2021-03-31 19:48:51 +0900

    Get rid of Consul

--- aws/res/bootstrap.sh
+++ aws/res/bootstrap.sh
@@ -4,9 +4,7 @@ set -euo pipefail; IFS=$'\n\t'
 # Enable verbose mode
 set -x
 
-CNI_VERSION=0.9.1
 NOMAD_VERSION=1.0.4
-CONSUL_VERSION=1.9.4
 
 #
 # ec2-instance-connect 를 제일 먼저 설치
@@ -106,56 +104,15 @@ usermod -a -G docker ec2-user
 # 이후 로그아웃한 뒤 재로그인
 
 #
-# CNI 설치
-#
-curl -L -o cni-plugins.tgz "https://github.com/containernetworking/plugins/releases/download/v${CNI_VERSION}/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz"
-mkdir -p /opt/cni/bin
-tar -C /opt/cni/bin -xzf cni-plugins.tgz
-rm cni-plugins.tgz
-
-#
 # Nomad 설치
 #
 curl "https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip" \
     -Lo /home/ec2-user/nomad_linux_amd64.zip
 unzip /home/ec2-user/nomad_linux_amd64.zip -d /usr/local/bin/
 rm /home/ec2-user/nomad_linux_amd64.zip
-mv /usr/local/bin/nomad /usr/local/bin/nomad.orig
-# Download swap-enabled nomad
-curl "https://github.com/lens0021/swappiness-nomad/releases/download/v1.0.4-swap/nomad" \
-    -Lo /usr/local/bin/nomad
 # Enable nomad autocompletion
 nomad -autocomplete-install
 complete -C /usr/local/bin/nomad nomad
-
-#
-# Consul 설치
-# Reference:
-#   - https://github.com/hashicorp/terraform-aws-consul/blob/master/modules/install-consul/install-consul
-#
-curl "https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip" \
-    -Lo /home/ec2-user/consul_linux_amd64.zip
-unzip /home/ec2-user/consul_linux_amd64.zip -d /usr/local/bin/
-rm /home/ec2-user/consul_linux_amd64.zip
-useradd consul
-chown -R consul:consul /usr/local/bin/consul
-chmod a+x /usr/local/bin/consul
-# Enable consul autocompletion
-consul -autocomplete-install
-complete -C /usr/bin/consul consul
-
-#
-# dnsmasq 설정
-# References:
-# - https://learn.hashicorp.com/tutorials/consul/dns-forwarding#dnsmasq-setup
-# - https://aws.amazon.com/premiumsupport/knowledge-center/dns-resolution-failures-ec2-linux
-#
-groupadd -r dnsmasq
-useradd -r -g dnsmasq dnsmasq
-sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-echo 'server=/consul/127.0.0.1#8600' >> /etc/dnsmasq.d/10-consul
-sudo systemctl restart dnsmasq.service
-sudo systemctl enable dnsmasq.service
 
 #
 # htoprc 생성
@@ -174,28 +131,24 @@ EOF
 # Clone Femiwiki Nomad configurations and specifications repository
 #
 sudo -u ec2-user git clone https://github.com/femiwiki/nomad.git /home/ec2-user/nomad/
-# Configure Nomad, Consul and systemd
+# Configure Nomad and systemd
 /home/ec2-user/nomad/up
 
 #
 # README 생성
 #
 sudo -u ec2-user tee /home/ec2-user/README <<'EOF' >/dev/null
-Nomad, Consul 관련 바이너리들
+바이너리들
 
     /usr/local/bin/nomad              nomad 바이너리
-    /usr/local/bin/consul             consul 바이너리
 
 systemd 유닛 파일
 
     /etc/systemd/system/nomad.service
-    /etc/systemd/system/consul.service
 
 기타 관련 파일들 위치
     /etc/nomad.d                                     Nomad configuration
-    /etc/consul.d                                    Consul configuration
     /opt/nomad                                       Nomad data directory
-    /opt/consul                                      Consul data directory
     /opt/aws/amazon-cloudwatch-agent/bin/config.json CloudWatch Agent Configuration File
 
 그 외 인스턴스가 어떻게 세팅되었는지는 아래 repo 참고
