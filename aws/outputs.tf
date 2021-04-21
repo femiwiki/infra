8e6f53e740262cc59539684c6a848774924a632d
lens0021 <lorentz0021@gmail.com>
2021-04-21 10:07:47 +0900

    aws: Set outputs for nomad cluster
    https://github.com/femiwiki/infra/issues/85

---
+++ aws/outputs.tf
@@ -0,0 +1,16 @@
+output "nomad_addr" {
+  description = "Address to our nomad cluster"
+  value       = "http://${aws_instance.femiwiki_arm64.public_ip}:4646"
+}
+
+output "ebs_mysql_id" {
+  value = aws_ebs_volume.persistent_data_mysql.id
+}
+
+output "ebs_caddycerts_id" {
+  value = aws_ebs_volume.persistent_data_caddycerts.id
+}
+
+output "ebs_secrets_id" {
+  value = aws_ebs_volume.persistent_data_secrets.id
+}
