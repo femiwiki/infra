24cf12f3a7670e23d1848a1b31790da1c4c6bb9b
lens0021 <lorentz0021@gmail.com>
2021-04-23 19:30:28 +0900

    bootstrap.sh: Install atop as per best practice
    Fixes https://github.com/femiwiki/femiwiki/issues/248

--- aws/res/bootstrap.sh
+++ aws/res/bootstrap.sh
@@ -31,10 +31,19 @@ yum install -y \
   jq \
   unzip \
   nc
-  # Disabled temporarily, See https://github.com/femiwiki/femiwiki/issues/248
-  # 'https://www.atoptool.nl/download/atop-2.4.0-1.x86_64.rpm'
   # Disabled temporarily, See https://github.com/femiwiki/femiwiki/issues/247
   # ripgrep
+
+#
+# Install atop
+# Reference: https://aws.amazon.com/premiumsupport/knowledge-center/ec2-linux-configure-monitoring-tools/#Amazon_Linux_2
+#
+amazon-linux-extras install -y epel
+sudo yum -y install sysstat atop --enablerepo=epel
+sudo sed -i 's/^LOGINTERVAL=600.*/LOGINTERVAL=60/' /etc/sysconfig/atop
+sudo sed -i -e 's|*/10|*/1|' -e 's|every 10 minutes|every 1 minute|' /etc/cron.d/sysstat
+sudo systemctl enable atop.service crond.service sysstat.service
+sudo systemctl restart atop.service crond.service sysstat.service
 
 #
 # yum-cron 설치
