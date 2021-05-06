45f590661d93188a37bf86cc643c93b81c9d5ee8
lens0021 <lorentz0021@gmail.com>
2021-05-06 22:13:38 +0900

    aws: Cleanup unusage ami data

--- aws/ami.tf
+++ aws/ami.tf
@@ -1,30 +1,11 @@
-data "aws_ami" "amazon_linux_2" {
-  owners      = ["amazon"]
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-minimal-hvm-2.0.*-x86_64-ebs"]
-  }
-
-  filter {
-    name   = "ena-support"
-    values = ["true"]
-  }
-
-  # 이 AMI로 고정함
-  # You can get latest image name by executing (with AWS CLI v1)
-  #   aws ec2 describe-images --filters "Name=name,Values=amzn2-ami-minimal-hvm-2.0.*-x86_64-ebs" --query 'sort_by(Images, &CreationDate)[::-1].[Name]'
-  name_regex = "^amzn2-ami-minimal-hvm-2.0.20201218.1-x86_64-ebs$"
-}
-
 data "aws_ami" "amazon_linux_2_arm64" {
   owners      = ["amazon"]
   most_recent = true
 
   filter {
     name = "name"
-    # aws ec2 describe-images --filters "Name=name,Values=amzn2-ami-minimal-hvm-2.0.*-arm64-ebs" --query 'sort_by(Images, &CreationDate)[::-1].[Name]'
+    # You can get latest image name by executing (with AWS CLI v1)
+    #   aws ec2 describe-images --filters "Name=name,Values=amzn2-ami-minimal-hvm-2.0.*-arm64-ebs" --query 'sort_by(Images, &CreationDate)[::-1].[Name]'
     values = ["amzn2-ami-minimal-hvm-2.0.*-arm64-ebs"]
   }
 
