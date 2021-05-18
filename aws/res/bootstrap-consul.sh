26b6f18d99bf7f4c0c2dce7f3e8593748a8539c1
lens0021 <lorentz0021@gmail.com>
2021-05-19 01:46:58 +0900

    Bump Nomad from 1.0.4 to 1.1.0
    https://github.com/femiwiki/femiwiki/issues/258

--- aws/res/bootstrap-consul.sh
+++ aws/res/bootstrap-consul.sh
@@ -5,7 +5,7 @@ set -euo pipefail; IFS=$'\n\t'
 set -x
 
 CNI_VERSION=0.9.1
-NOMAD_VERSION=1.0.4
+NOMAD_VERSION=1.1.0
 CONSUL_VERSION=1.9.5
 
 #
--- aws/res/bootstrap.sh
+++ aws/res/bootstrap.sh
@@ -5,7 +5,7 @@ set -euo pipefail; IFS=$'\n\t'
 set -x
 
 CNI_VERSION=0.9.1
-NOMAD_VERSION=1.0.4
+NOMAD_VERSION=1.1.0
 
 #
 # ec2-instance-connect 를 제일 먼저 설치
