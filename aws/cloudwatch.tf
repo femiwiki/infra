43f5e62509c552f7c9c4e95871e2070bf4fcabac
lens0021 <lorentz0021@gmail.com>
2021-06-24 10:08:16 +0900

    aws: Increase datapoints_to_alarm of volume_idle_time alarm

--- aws/cloudwatch.tf
+++ aws/cloudwatch.tf
@@ -7,10 +7,10 @@ resource "aws_cloudwatch_metric_alarm" "femiwiki_cpu_credit_balance_cloud_watch_
   dimensions = {
     InstanceId = aws_instance.femiwiki.id
   }
-  evaluation_periods  = "1"
-  threshold           = "72"
+  threshold           = 72
   comparison_operator = "LessThanThreshold"
   datapoints_to_alarm = 1
+  evaluation_periods  = 1
   alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic.arn]
 }
 
@@ -23,10 +23,10 @@ resource "aws_cloudwatch_metric_alarm" "femiwiki_volume_idle_time_cloud_watch_al
   dimensions = {
     VolumeId = aws_instance.femiwiki.root_block_device[0].volume_id
   }
-  evaluation_periods  = "1"
-  threshold           = "20"
+  threshold           = 20
   comparison_operator = "LessThanThreshold"
-  datapoints_to_alarm = 1
+  datapoints_to_alarm = 2
+  evaluation_periods  = 2
   alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic.arn]
 }
 
@@ -42,10 +42,10 @@ resource "aws_cloudwatch_metric_alarm" "femiwiki_disk_used_cloud_watch_alarm" {
     "host"   = aws_instance.femiwiki.private_dns
     "path"   = "/"
   }
-  evaluation_periods  = "1"
-  threshold           = "90"
+  threshold           = 90
   comparison_operator = "GreaterThanThreshold"
   datapoints_to_alarm = 1
+  evaluation_periods  = 1
   alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic.arn]
 }
 
@@ -61,10 +61,10 @@ resource "aws_cloudwatch_metric_alarm" "persistent_data_mysql_used_cloud_watch_a
     "host"   = aws_instance.femiwiki.private_dns
     "path"   = "/opt/nomad/client/csi/node/aws-ebs0/staging/mysql/rw-file-system-single-node-writer"
   }
-  evaluation_periods  = "1"
-  threshold           = "90"
+  threshold           = 90
   comparison_operator = "GreaterThanThreshold"
   datapoints_to_alarm = 1
+  evaluation_periods  = 1
   alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic.arn]
 }
 
@@ -76,10 +76,10 @@ resource "aws_cloudwatch_metric_alarm" "bounce_rate" {
   metric_name         = "Reputation.BounceRate"
   period              = 300
   statistic           = "Average"
-  evaluation_periods  = "1"
-  threshold           = "0.05"
+  threshold           = 0.05
   comparison_operator = "GreaterThanOrEqualToThreshold"
   datapoints_to_alarm = 1
+  evaluation_periods  = 1
   alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic_us.arn]
   treat_missing_data  = "ignore"
 }
@@ -92,10 +92,10 @@ resource "aws_cloudwatch_metric_alarm" "complaint_rate" {
   metric_name         = "Reputation.ComplaintRate"
   period              = 300
   statistic           = "Average"
-  evaluation_periods  = "1"
-  threshold           = "0.001"
+  threshold           = 0.001
   comparison_operator = "GreaterThanOrEqualToThreshold"
   datapoints_to_alarm = 1
+  evaluation_periods  = 1
   alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic_us.arn]
   treat_missing_data  = "ignore"
 }
@@ -111,10 +111,10 @@ resource "aws_cloudwatch_metric_alarm" "femiwiki_main_page" {
   dimensions = {
     HealthCheckId = aws_route53_health_check.femiwiki_main_page.id
   }
-  evaluation_periods  = "1"
   threshold           = 1
   comparison_operator = "LessThanThreshold"
   datapoints_to_alarm = 1
+  evaluation_periods  = 1
   alarm_actions       = [aws_sns_topic.cloudwatch_alarms_topic_us.arn]
   ok_actions          = [aws_sns_topic.cloudwatch_alarms_topic_us.arn]
 }
