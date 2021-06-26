ea7c0c2d1bc3c556b20d21fd6d633edfe15f2d84
Lens0021 <lorentz0021@gmail.com>
2021-06-27 06:08:55 +0900

    Reduce swap from 1.5G to 512M
    Closes https://github.com/femiwiki/femiwiki/issues/280

--- aws/res/bootstrap.sh
+++ aws/res/bootstrap.sh
@@ -97,7 +97,7 @@ EOF
 # 스왑 메모리 생성
 # 아마존 리눅스에서는 기본으로 XFS를 쓰는데, 이 경우 fallocate 명령어를 쓰지 못한다.
 #
-sudo dd if=/dev/zero of=/swapfile bs=256M count=6
+sudo dd if=/dev/zero of=/swapfile bs=256M count=2
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
