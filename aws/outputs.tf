130bdb0942c1dda16eebbf7bb8c10c8f27f7d294
lens0021 <lorentz0021@gmail.com>
2021-07-03 13:59:14 +0900

    aws: Add consul test output

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -3,10 +3,10 @@ output "nomad_addr" {
   value       = "http://${aws_instance.femiwiki.public_ip}:4646"
 }
 
-# output "nomad_addr_consul_test" {
-#   description = "Address to our nomad cluster for consul test"
-#   value       = "http://${aws_instance.femiwiki_consul_test.public_ip}:4646"
-# }
+output "nomad_addr_consul_test" {
+  description = "Address to our nomad cluster for consul test"
+  value       = "http://${aws_instance.femiwiki_consul_test_server.public_ip}:4646"
+}
 
 output "ebs_mysql_id" {
   value = aws_ebs_volume.persistent_data_mysql.id
