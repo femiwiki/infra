514fc43e2818c350981976d6d2a97c24def160d7
lens0021 <lorentz0021@gmail.com>
2021-08-14 21:56:28 +0900

    Remove internal nomad ports; we have only one instance

--- aws/sg-nomad.tf
+++ aws/sg-nomad.tf
@@ -19,27 +19,3 @@ resource "aws_security_group_rule" "femiwiki_ingress_nomad" {
   cidr_blocks      = ["0.0.0.0/0"]
   ipv6_cidr_blocks = ["::/0"]
 }
-
-# Nomad ports
-#   Reference: https://www.nomadproject.io/docs/install/production/requirements#ports-used
-resource "aws_security_group_rule" "femiwiki_ingress_internal_nomad" {
-  security_group_id = aws_security_group.nomad_cluster.id
-  description       = "Nomad"
-
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = 4646
-  to_port                  = 4648
-  source_security_group_id = aws_security_group.nomad_cluster.id
-}
-
-resource "aws_security_group_rule" "femiwiki_ingress_internal_nomad_dynamic" {
-  security_group_id = aws_security_group.nomad_cluster.id
-  description       = "Nomad dynamic ports"
-
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = 20000
-  to_port                  = 32000
-  source_security_group_id = aws_security_group.nomad_cluster.id
-}
