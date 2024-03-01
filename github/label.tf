5668b074b8ef2ad24ac7d2abe5d1718d17b18578
Lens0021 / Leslie <lorentz0021@gmail.com>
2024-03-01 15:09:16 +0900

    Use extension labels for OOUIFemiwikiTheme

--- github/label.tf
+++ github/label.tf
@@ -326,7 +326,7 @@ locals {
     caddy-mwcache     = local.label_suite.base
     legunto           = local.label_suite.base
     maintenance       = local.label_suite.base
-    OOUIFemiwikiTheme = local.label_suite.base
+    OOUIFemiwikiTheme = local.label_suite.base_ext
     remote-gadgets    = concat(local.label_suite.base, ["search", "windows", "mw1_38", "mw1_39", "mw1_40", "mw1_41"])
 
     backupbot  = local.label_suite.base
