a3282d8fc160ca170cbebd75340daae433b640a4
lens0021 <lorentz0021@gmail.com>
2024-03-02 02:23:16 +0900

    Amazon Linux 2023

--- aws/ami.tf
+++ aws/ami.tf
@@ -5,8 +5,8 @@ data "aws_ami" "amazon_linux_2_arm64" {
   filter {
     name = "name"
     # You can get latest image name by executing (with AWS CLI v1)
-    #   aws ec2 describe-images --filters "Name=name,Values=amzn2-ami-minimal-hvm-2.0.*-arm64-ebs" --query 'sort_by(Images, &CreationDate)[::-1].[Name]'
-    values = ["amzn2-ami-minimal-hvm-2.0.*-arm64-ebs"]
+    #   aws ec2 describe-images --filters "Name=name,Values=al2023-ami-minimal-*-*-*-arm64" --query 'sort_by(Images, &CreationDate)[::-1].[Name]'
+    values = ["al2023-ami-minimal-*-*-*-arm64"]
   }
 
   filter {
