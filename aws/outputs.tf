3374bd70ff8890498150ed15637f4e21bfc17410
lens0021 <lorentz0021@gmail.com>
2024-10-06 14:49:12 +0900

    aws: Add test_nomad_private_ip

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -8,6 +8,11 @@ output "test_nomad_addr" {
   value       = aws_eip.test_femiwiki.public_ip
 }
 
+output "test_nomad_private_ip" {
+  description = "Address to our nomad cluster"
+  value       = aws_instance.test_femiwiki.public_ip
+}
+
 output "ebs_mysql_id" {
   value = aws_ebs_volume.persistent_data_mysql.id
 }
