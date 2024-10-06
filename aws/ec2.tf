a22cc5159f7a3bcc844dd1bb53f11c07c69b586e
lens0021 <lorentz0021@gmail.com>
2024-10-06 20:59:41 +0900

    aws: Create an EBS volume for MySQL replica

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -38,6 +38,15 @@ resource "aws_ebs_volume" "persistent_data_mysql" {
   }
 }
 
+resource "aws_ebs_volume" "persistent_data_mysql_green" {
+  availability_zone = data.aws_availability_zone.femiwiki.name
+  type              = "gp3"
+  size              = 8
+  tags = {
+    Name = "Mysql data directory for Main Server"
+  }
+}
+
 resource "aws_ebs_volume" "persistent_data_caddycerts" {
   availability_zone = data.aws_availability_zone.femiwiki.name
   type              = "gp3"
--- aws/iam-policies.tf
+++ aws/iam-policies.tf
@@ -216,6 +216,7 @@ data "aws_iam_policy_document" "mount_ebs_volumes" {
     resources = concat([
       aws_instance.femiwiki.arn,
       aws_ebs_volume.persistent_data_mysql.arn,
+      aws_ebs_volume.persistent_data_mysql_green.arn,
       aws_ebs_volume.persistent_data_caddycerts.arn,
       aws_ebs_volume.persistent_data_caddycerts_green.arn,
       ], [for instance in aws_instance.femiwiki_green : instance.arn]
