52e21464360c7e5f59a5c0fa3770df02c8b2109a
Lens0021 / Leslie <lorentz0021@gmail.com>
2024-10-07 00:08:19 +0900

    aws: Resize the Amazon EBS volume to 16G (#342)

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -159,7 +159,7 @@ resource "aws_instance" "femiwiki_green" {
 
   root_block_device {
     delete_on_termination = true
-    volume_size           = 8
+    volume_size           = 16
     volume_type           = "gp3"
   }
 
