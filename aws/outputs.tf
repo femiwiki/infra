1047650abf383060ae52e7b96ca2fa77ebb60658
lens0021 <lorentz0021@gmail.com>
2025-08-25 23:59:11 +0900

    Trim pem outputs

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -3,14 +3,14 @@ output "blue_public_ip" {
 }
 
 output "client_ca_cert_pem" {
-  value = tls_self_signed_cert.ca_cert.cert_pem
+  value = trimspace(tls_self_signed_cert.ca_cert.cert_pem)
 }
 
 output "client_cert_pem" {
-  value = tls_locally_signed_cert.client_cert.cert_pem
+  value = trimspace(tls_locally_signed_cert.client_cert.cert_pem)
 }
 
 output "client_key_pem" {
-  value     = tls_private_key.client_key.private_key_pem
+  value     = trimspace(tls_private_key.client_key.private_key_pem)
   sensitive = true
 }
