5bb95ea88f7c15d686537650006a1236f37af79a
Lens0021 / Leslie <lorentz0021@gmail.com>
2025-08-31 16:48:35 +0900

    aws: Allow more ips for docker tls (#432)
    * aws: Allow more ips for docker tls
    
    * aws: New instance

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -137,6 +137,7 @@ resource "aws_instance" "database" {
   ami                         = data.aws_ami.amazon_linux_2_arm64.image_id
   availability_zone           = data.aws_availability_zone.femiwiki.name
   disable_api_termination     = true
+  disable_api_stop            = true
   ebs_optimized               = true
   iam_instance_profile        = aws_iam_instance_profile.database.name
   instance_type               = "t4g.small"
@@ -189,3 +190,62 @@ resource "aws_instance" "database" {
     ]
   }
 }
+
+resource "aws_instance" "docker" {
+  ami                         = data.aws_ami.amazon_linux_2_arm64.image_id
+  availability_zone           = data.aws_availability_zone.femiwiki.name
+  disable_api_termination     = true
+  disable_api_stop            = true
+  ebs_optimized               = true
+  iam_instance_profile        = aws_iam_instance_profile.femiwiki.name
+  instance_type               = "t4g.micro"
+  monitoring                  = false
+  user_data_replace_on_change = false
+
+  user_data = templatefile("res/user-data-docker-provider.tftpl", {
+    alloy_config = templatefile("res/config.alloy.tftpl", {
+      name                = "femiwiki"
+      prometheus_endpoint = "https://prometheus-prod-49-prod-ap-northeast-0.grafana.net/api/prom/push"
+      prometheus_username = "1835631"
+      prometheus_password = var.prometheus_password
+      loki_endpoint       = "https://logs-prod-030.grafana.net/loki/api/v1/push"
+      loki_username       = "1017101"
+      loki_password       = var.loki_password
+    })
+    ca_cert_pem     = tls_self_signed_cert.ca_cert.cert_pem
+    server_cert_pem = tls_locally_signed_cert.server_cert.cert_pem
+    server_key_pem  = tls_private_key.server_key.private_key_pem
+  })
+
+  vpc_security_group_ids = [
+    aws_default_security_group.default.id,
+    aws_security_group.femiwiki.id,
+    aws_security_group.mediawiki.id,
+  ]
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 32
+    volume_type           = "gp3"
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
+  metadata_options {
+    instance_metadata_tags = "enabled"
+  }
+
+  tags = {
+    Name = "docker"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      ami,
+      user_data,
+    ]
+  }
+}
+
--- aws/eip.tf
+++ aws/eip.tf
@@ -1,7 +1,6 @@
 resource "aws_eip" "femiwiki" {
-  instance = aws_instance.femiwiki.id
-  domain   = "vpc"
-  tags     = { Name = "femiwiki.com" }
+  domain = "vpc"
+  tags   = { Name = "femiwiki.com" }
 }
 
 resource "aws_eip_association" "femiwiki" {
@@ -15,9 +14,8 @@ resource "aws_eip" "test_femiwiki" {
 }
 
 resource "aws_eip_association" "test_femiwiki" {
-  count         = 0
   allocation_id = aws_eip.test_femiwiki.id
-  instance_id   = aws_instance.femiwiki_blue.id
+  instance_id   = aws_instance.docker.id
 }
 
 
