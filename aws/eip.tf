fcb647c5db5ae4094bd88369ac55f58f68aab410
lens0021 <lorentz0021@gmail.com>
2025-08-31 15:50:17 +0900

    aws: change the target of femiwiki.com

--- aws/eip.tf
+++ aws/eip.tf
@@ -6,7 +6,7 @@ resource "aws_eip" "femiwiki" {
 
 resource "aws_eip_association" "femiwiki" {
   allocation_id = aws_eip.femiwiki.id
-  instance_id   = aws_instance.femiwiki.id
+  instance_id   = aws_instance.femiwiki_blue.id
 }
 
 resource "aws_eip" "test_femiwiki" {
@@ -15,6 +15,7 @@ resource "aws_eip" "test_femiwiki" {
 }
 
 resource "aws_eip_association" "test_femiwiki" {
+  count         = 0
   allocation_id = aws_eip.test_femiwiki.id
   instance_id   = aws_instance.femiwiki_blue.id
 }
