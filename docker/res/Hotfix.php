0c3a1b39337a766538705244b6218f902664404e
lens0021 <lorentz0021@gmail.com>
2025-08-31 15:19:56 +0900

    Import Hotfix.php
    https://github.com/femiwiki/nomad/blob/53db0b117e788572b43c6c45930cb883fe423071/jobs/fastcgi.nomad#L240-L257
    https://github.com/femiwiki/infra/blob/fa2ede88a3d51635dce041824aae100075d448e4/aws/res/user-data-orchestratorless.tftpl#L149-L189

---
+++ docker/res/Hotfix.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Use this file for hotfixes
+ *
+ * @file
+ */
+
+$wgAutoConfirmAge = 3600;
+$wgUnifiedExtensionForFemiwikiBlockByEmail = false;
+$wgUnifiedExtensionForFemiwikiRelatedArticlesUseLinks = false;
+
+foreach ( [
+	NS_TALK,
+	NS_USER_TALK,
+	NS_PROJECT_TALK,
+	NS_FILE_TALK,
+	NS_MEDIAWIKI_TALK,
+	NS_TEMPLATE_TALK,
+	NS_HELP_TALK,
+	NS_CATEGORY_TALK,
+	NS_ITEM_TALK,
+	NS_PROPERTY_TALK,
+	NS_WIDGET_TALK,
+	NS_MODULE_TALK,
+	NS_TRANSLATIONS_TALK,
+	NS_GADGET_TALK,
+	NS_GADGET_DEFINITION_TALK,
+	NS_NEWSLETTER_TALK,
+	NS_BBS,
+	NS_BBS_TALK,
+] as $space ) {
+	$wgNamespaceContentModels[$space] = CONTENT_MODEL_WIKITEXT;
+}
+
+// Maintenance
+// 점검이 끝나면 아래 라인 주석처리한 뒤, 아래 문서 내용을 비우면 됨
+// https://femiwiki.com/w/%EB%AF%B8%EB%94%94%EC%96%B4%EC%9C%84%ED%82%A4:Sitenotice
+// $wgReadOnly = '데이터베이스 업그레이드 작업이 진행 중입니다. 작업이 진행되는 동안 사이트 이용이 제한됩니다.';
+
+// 업로드를 막고싶을때엔 아래 라인 주석 해제하면 됨
+// $wgEnableUploads = false;
