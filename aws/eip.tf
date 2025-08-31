4c82991e4053bb6579ba3694a02216efeec7dc31
Lens0021 / Leslie <lorentz0021@gmail.com>
2025-08-31 16:58:38 +0900

    Use new instance (#433)

--- aws/eip.tf
+++ aws/eip.tf
@@ -5,7 +5,7 @@ resource "aws_eip" "femiwiki" {
 
 resource "aws_eip_association" "femiwiki" {
   allocation_id = aws_eip.femiwiki.id
-  instance_id   = aws_instance.femiwiki_blue.id
+  instance_id   = aws_instance.docker.id
 }
 
 resource "aws_eip" "test_femiwiki" {
@@ -14,6 +14,7 @@ resource "aws_eip" "test_femiwiki" {
 }
 
 resource "aws_eip_association" "test_femiwiki" {
+  count         = 0
   allocation_id = aws_eip.test_femiwiki.id
   instance_id   = aws_instance.docker.id
 }
