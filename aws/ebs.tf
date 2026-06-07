43fe3cee2f7b1f3299f35c0df1f57dafc68e5a21
Lens0021 / Leslie <lorentz0021@gmail.com>
2026-06-07 21:10:09 +0900

    aws: Increase EBS volume size from 8GB to 32GB

--- aws/ebs.tf
+++ aws/ebs.tf
@@ -1,7 +1,7 @@
 resource "aws_ebs_volume" "persistent_data_mysql_2" {
   availability_zone = data.aws_availability_zone.femiwiki.name
   type              = "gp3"
-  size              = 8
+  size              = 32
   tags = {
     Name = "MySQL data directory for MySQL server_id = 2"
   }
