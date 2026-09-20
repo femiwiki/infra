2a09985461d358836ba956b160d2a271741bfd5a
lens0021 <lorentz0021@gmail.com>
2026-09-20 21:32:30 +0900

    docker: start Caddy through caddy-run

--- docker/container.tf
+++ docker/container.tf
@@ -1,7 +1,7 @@
 resource "docker_container" "http" {
   name            = "http-${local.fastcgi_generation}"
   image           = "ghcr.io/femiwiki/femiwiki:2026-09-20T12-32-42e59e77"
-  command         = ["caddy", "run"]
+  command         = ["caddy-run"]
   restart         = "on-failure"
   max_retry_count = 3
   network_mode    = "host"
