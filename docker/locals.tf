72399e8603803acf1d6926d65e1efb4913aaa2e2
lens0021 <lorentz0021@gmail.com>
2026-09-20 18:15:55 +0900

    docker: fastcgi generation 2

--- docker/locals.tf
+++ docker/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  fastcgi_generation = 1
+  fastcgi_generation = 2
 
   ssm_parameters_mediawiki = {
     for i, name in data.terraform_remote_state.aws.outputs.ssm_parameters_mediawiki.names :
