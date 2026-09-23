77ea38377c71075bd593c90239451a7c0d0818ee
lens0021 <28209361+lens0021@users.noreply.github.com>
2026-09-23 14:37:40 +0000

    build(docker): bump femiwiki image to 2026-09-23T14-37-bf16c29e

--- docker/container.tf
+++ docker/container.tf
@@ -1,6 +1,6 @@
 resource "docker_container" "http" {
   name         = "http-${local.fastcgi_generation}"
-  image        = "ghcr.io/femiwiki/femiwiki:2026-09-22T01-02-4b0a06bb"
+  image        = "ghcr.io/femiwiki/femiwiki:2026-09-23T14-37-bf16c29e"
   command      = ["caddy-run"]
   restart      = "always"
   network_mode = "host"
@@ -70,7 +70,7 @@ resource "docker_container" "http" {
 
 resource "docker_container" "fastcgi" {
   name         = "fastcgi-${local.fastcgi_generation}"
-  image        = "ghcr.io/femiwiki/femiwiki:2026-09-22T01-02-4b0a06bb"
+  image        = "ghcr.io/femiwiki/femiwiki:2026-09-23T14-37-bf16c29e"
   network_mode = "host"
   restart      = "always"
 
--- docker/locals.tf
+++ docker/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  fastcgi_generation = 22
+  fastcgi_generation = 24
 }
